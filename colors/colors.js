74404e2dfbd68d1656cb1ccba6c09af367214aaa
Logan Masniuk <logan.masniuk@gmail.com>
2024-01-26 11:49:26 -0800

    PLay around with box shadows

--- colors/colors.js
+++ colors/colors.js
@@ -3,4 +3,22 @@ function changeColor(){
         // Generate a random color
     const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
     div.style.backgroundColor = randomColor;
+    const boxShadowValue = `10px 10px 0px 0px ${hexToRGBA(randomColor,0.5)}`;
+    div.style.boxShadow = boxShadowValue;
+}
+
+
+function hexToRGBA(hex, alpha) {
+    // Remove the hash sign if it's included
+    hex = hex.replace('#', '');
+
+    // Parse the hex values
+    var r = parseInt(hex.substring(0, 2), 16);
+    var g = parseInt(hex.substring(2, 4), 16);
+    var b = parseInt(hex.substring(4, 6), 16);
+
+    // Create the RGBA string
+    var rgba = 'rgba(' + r + ', ' + g + ', ' + b + ', ' + alpha + ')';
+
+    return rgba;
 }
